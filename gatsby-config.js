20532ad4d43b45e0c325f52e0d224a710bf044a2
kazumoto <motch0214@gmail.com>
2020-11-30 23:52:44 +0900

    chore: Bundle analyser

--- gatsby-config.js
+++ gatsby-config.js
@@ -86,5 +86,11 @@ module.exports = {
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
+    {
+      resolve: `gatsby-plugin-webpack-bundle-analyser-v2`,
+      options: {
+        disable: !(process.env.ENABLE_BUNDLE_ANALYSER === "true"),
+      },
+    },
   ],
 }
