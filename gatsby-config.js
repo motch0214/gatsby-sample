fb49445aeb5632f98b07477846d51e0983ed67b0
kazumoto <motch0214@gmail.com>
2020-11-30 14:28:21 +0900

    feat: Base URL for import

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,8 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 require("dotenv").config()
 
+const path = require("path")
+
 module.exports = {
   siteMetadata: {
     title: `Gatsby Default Starter`,
@@ -8,6 +10,12 @@ module.exports = {
     author: `@gatsbyjs`,
   },
   plugins: [
+    {
+      resolve: "gatsby-plugin-root-import",
+      options: {
+        components: path.join(__dirname, "src/components"),
+      },
+    },
     `gatsby-plugin-postcss`,
     {
       resolve: `gatsby-plugin-material-ui`,
