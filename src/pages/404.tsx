030d1315c43fa2f0ee899a930ee036b15147b71c
kazumoto <motch0214@gmail.com>
2020-12-02 13:24:07 +0900

    fix: Meta of 404 page

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,12 +1,14 @@
 import { navigate } from "gatsby"
 import React, { useEffect } from "react"
 
+import SEO from "components/seo"
+
 const NotFoundPage: React.FC = () => {
   useEffect(() => {
     navigate("/")
   }, [])
 
-  return null
+  return <SEO title="Not found" />
 }
 
 export default NotFoundPage
